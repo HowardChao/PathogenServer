90ae520fbc5c1992524b4968c0d3e6d9984ec9e6
Chao Kuan-Hao <ntueeb05howard@gmail.com>
2019-01-29 12:58:26 +0800

    Update files

--- src/VirusRNASeq/dataanalysis/views.py
+++ src/VirusRNASeq/dataanalysis/views.py
@@ -1,3 +1,6 @@
+from django.core.cache import cache
+from django.http import HttpResponse
+import json
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect
 from django.core.files.storage import FileSystemStorage
@@ -33,5 +36,26 @@ def model_form_upload(request):
         'form': form
     })
 
+
 def hello_world(request):
     return HttpResponse('Hello World!')
+
+# -*- coding: utf-8 -*-
+
+def upload_progress(request):
+    """
+    Used by Ajax calls
+
+    Return the upload progress and total length values
+    """
+    if 'X-Progress-ID' in request.GET:
+        progress_id = request.GET['X-Progress-ID']
+    elif 'X-Progress-ID' in request.META:
+        progress_id = request.META['X-Progress-ID']
+    else:
+        progress_id = None
+
+    if progress_id:
+        cache_key = "%s_%s" % (request.META['REMOTE_ADDR'], progress_id)
+        data = cache.get(cache_key)
+        return HttpResponse(json.dumps(data))
