912a0d9f353524e8d12371f754f34b77b46eff89
Chao Kuan-Hao <chaokuan-hao@Chaode-MacBook-Pro-4.local>
2019-01-29 12:58:26 +0800

    Update files

--- src/VirusRNASeq/dataanalysis/urls.py
+++ src/VirusRNASeq/dataanalysis/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('upload/simple/', views.simple_upload, name='simple_upload'),
     path('upload/form/', views.model_form_upload, name='model_form_upload'),
+    path('upload_progress/', views.upload_progress, name='upload_progress')
 ]
 
 if settings.DEBUG:
