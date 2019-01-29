37fa4761da34b5ed360520c4da895d511adb006c
Chao Kuan-Hao <ntueeb05howard@gmail.com>
2019-01-29 12:58:26 +0800

    Update files

---
+++ src/VirusRNASeq/email_hash/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from crispy_forms.helper import FormHelper
+from email_hash import models
+
+class NewsletterUserSignUpForm(forms.ModelForm):
+    helper = FormHelper()
+    helper.form_show_labels = False
+    class Meta:
+        model = models.NewsletterUser
+        fields = ['email']
+
+        def clean_email(self):
+            email = self.clean_data.get('email')
+            return email
