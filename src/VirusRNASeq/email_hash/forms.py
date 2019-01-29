c757c6964c7561b0f6a0fcc3d0911fa35982bd4e
Kuan-Hao <ntueeb05howard@gmail.com>
2019-01-30 03:53:01 +0800

    Update user login

--- src/VirusRNASeq/email_hash/forms.py
+++ src/VirusRNASeq/email_hash/forms.py
@@ -1,25 +1,57 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from crispy_forms.helper import FormHelper
+from django.contrib.auth.models import User
 from email_hash import models
+import uuid
 
 class NewsletterUserSignUpForm(forms.ModelForm):
     helper = FormHelper()
     helper.form_show_labels = False
     # helper.form_class = 'blueForms'
     # helper.update_attributes(maxlength="400")
-
     class Meta:
         model = models.NewsletterUser
-        fields = ['email']
+        fields = ['project_name', 'email']
+
+        def clean_project_name(self):
+            project_name = self.clean_data.get('project_name')
+            return project_name
         def clean_email(self):
             email = self.clean_data.get('email')
             return email
 
 
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
 
+class UserRegisterForm(UserCreationForm):
+    project_name = forms.CharField(
+        max_length=100)
+    email = forms.EmailField()
+    # analysis_code = forms.CharField(
+    #     max_length=32)
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['project_name', 'email']
+
+    def save(self, commit=True):
+        user = super(UserRegisterForm, self).save(commit=False)
+        user.username = self.cleaned_data["project_name"]
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        # self.fields['password1'].required = False
+        # self.fields['password2'].required = False
+        # If one field gets autocompleted but not the other, our 'neither
+        # password or both password' validation will be triggered.
+        # self.fields['password1'].widget.attrs['autocomplete'] = 'off'
+        # self.fields['password2'].widget.attrs['autocomplete'] = 'off'
+        del self.fields['password1']
+        del self.fields['password2']
+        # If one field gets autocompleted but not the other, our 'neither
+        # password or both password' validation will be triggered.
+        # self.fields['password1'].widget.attrs['autocomplete'] = 'off'
+        # self.fields['password2'].widget.attrs['autocomplete'] = 'off'
--- src/VirusRNASeq/email_hash/models.py
+++ src/VirusRNASeq/email_hash/models.py
@@ -1,12 +1,17 @@
 from django.db import models
-from django.db.models.signals import pre_init
+from django.contrib.auth.models import User
 import uuid
 
 def analysis_code_generator():
     return uuid.uuid1().hex
 
-class NewsletterUser(models.Model):
 
+def project_name_generator():
+    return "Project" + uuid.uuid1().hex
+
+class NewsletterUser(models.Model):
+    project_name = models.CharField(
+        max_length=100, default=project_name_generator)
     email = models.EmailField()
     data_added = models.DateTimeField(auto_now_add=True)
     analysis_code = models.CharField(
