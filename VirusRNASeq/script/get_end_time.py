3b847e91f3619aa97683e6320d50069817f70bfc
Kuan-Hao <ntueeb05howard@gmail.com>
2019-03-03 16:41:08 +0800

    Update get last time

--- VirusRNASeq/script/get_end_time.py
+++ VirusRNASeq/script/get_end_time.py
@@ -3,6 +3,6 @@ import datetime
 get_time = datetime.datetime.now() 
 print(get_time)
 get_time_strip = get_time.strftime("%B %d, %Y, %I:%M:%S %p")
-f_get = open("start_time.txt", 'w')
+f_get = open("end_time.txt", 'w')
 f_get.writelines(get_time_strip)
 f_get.close()
