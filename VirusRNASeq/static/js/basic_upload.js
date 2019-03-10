94d26e4434f72c0afc5b45f10d460484f40dcfc2
HowardChao <ntueeb05howard@gmail.com>
2019-03-10 18:39:05 +0800

    Update file upload

--- VirusRNASeq/static/js/basic_upload.js
+++ VirusRNASeq/static/js/basic_upload.js
@@ -1,31 +1,68 @@
 $(function () {
 
   $(".js-upload-photos").click(function () {
+
     $("#fileupload").click();
   });
+
+
+
+
+  // $(function () {
+  //     $('#fileupload').fileupload({
+  //         dataType: 'json',
+  //         add: function (e, data) {
+  //             data.context = $('<button/>').text('Upload')
+  //                 .appendTo(document.body)
+  //                 .click(function () {
+  //                     data.context = $('<p/>').text('Uploading...').replaceAll($(this));
+  //                     data.submit();
+  //                 });
+  //         },
+  //         done: function (e, data) {
+  //             data.context.text('Upload finished.');
+  //         }
+  //     });
+  // });
+
+
+
 
   $("#fileupload").fileupload({
    dataType: 'json',
    sequentialUploads: true,  /* 1. SEND THE FILES ONE BY ONE */
-   start: function (e) {  /* 2. WHEN THE UPLOADING PROCESS STARTS, SHOW THE MODAL */
-     $("#modal-progress").modal("show");
+
+
+       // data.context = $('press-to-upload').text('Upload')
+       //     .appendTo(document.body)
+       //     .click(function () {
+       //         data.context = $('<p/>').text('Uploading...').replaceAll($(this));
+       //         data.submit();
+       //     });
+   add: function (e, data) {
+       $("#up_btn").unbind('click').on('click', function () {
+           data.submit();
+       });
    },
-   stop: function (e) {  /* 3. WHEN THE UPLOADING PROCESS FINALIZE, HIDE THE MODAL */
-     $("#modal-progress").modal("hide");
-   },
-   progressall: function (e, data) {  /* 4. UPDATE THE PROGRESS BAR */
-     var progress = parseInt(data.loaded / data.total * 100, 10);
-     var strProgress = progress + "%";
-     $(".progress-bar").css({"width": strProgress});
-     $(".progress-bar").text(strProgress);
-   },
-   done: function (e, data) {
-     if (data.result.is_valid) {
-       $("#gallery tbody").prepend(
-         "<tr><td><a href='" + data.result.url + "'>" + data.result.name + "</a></td></tr>"
-       )
-     }
-   }
+   // start: function (e) {  /* 2. WHEN THE UPLOADING PROCESS STARTS, SHOW THE MODAL */
+   //   $("#modal-progress").modal("show");
+   // },
+   // stop: function (e) {  /* 3. WHEN THE UPLOADING PROCESS FINALIZE, HIDE THE MODAL */
+   //   $("#modal-progress").modal("hide");
+   // },
+   // progressall: function (e, data) {  /* 4. UPDATE THE PROGRESS BAR */
+   //   var progress = parseInt(data.loaded / data.total * 100, 10);
+   //   var strProgress = progress + "%";
+   //   $(".progress-bar").css({"width": strProgress});
+   //   $(".progress-bar").text(strProgress);
+   // },
+   // done: function (e, data) {
+   //   if (data.result.is_valid) {
+   //     $("#gallery tbody").prepend(
+   //       "<tr><td><a href='" + data.result.url + "'>" + data.result.name + "</a></td></tr>"
+   //     )
+   //   }
+   // }
 
  });
 
