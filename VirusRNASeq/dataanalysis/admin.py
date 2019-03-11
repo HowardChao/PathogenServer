3ed0cd415e05be665fd196aa5eb0f4e1d552c358
HowardChao <ntueeb05howard@gmail.com>
2019-03-11 08:18:09 +0800

    Change upload ifle

--- VirusRNASeq/dataanalysis/admin.py
+++ VirusRNASeq/dataanalysis/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from dataanalysis import models
 
 # Register your models here.
+class DataanalysisAdmin(admin.ModelAdmin):
+    list_display = ('title', 'file', 'uploaded_at')
+
+admin.site.register(models.Data, DataanalysisAdmin)
--- VirusRNASeq/dataanalysis/models.py
+++ VirusRNASeq/dataanalysis/models.py
@@ -24,3 +24,5 @@ class Data(models.Model):
     title = models.CharField(max_length=255, blank=True)
     file = models.FileField(upload_to='photos/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
