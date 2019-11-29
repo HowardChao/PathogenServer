8f0628d654ba3dcafe86e1ea60c18fd51f5f871e
Kuan-Hao Chao <ntueeb05howard@gmail.com>
2019-11-30 01:32:17 +0800

    Add celery into project

--- VirusRNASeq/VirusRNASeq/settings.py
+++ VirusRNASeq/VirusRNASeq/settings.py
@@ -182,11 +182,11 @@ Q_CLUSTER = {
 
 # celery settings
 
-CELERY_BROKER_URL = 'amqp://guest:guest@localhost'
+# CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
 #: Only add pickle to this list if your broker is secured
 #: from unwanted access (see userguide/security.html)
+CELERY_BROKER_URL = 'amqp://localhost'
 CELERY_ACCEPT_CONTENT = ['json']
-CELERY_RESULT_BACKEND = 'db+sqlite:///results.sqlite'
-CELERY_TASK_SERIALIZER = 'json'
-
+CELERY_RESULT_BACKEND = 'django-db'
 CELERY_CACHE_BACKEND = 'django-cache'
+CELERY_TASK_SERIALIZER = 'json'
