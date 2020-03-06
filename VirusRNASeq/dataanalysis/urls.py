75f90a1c8d8bf7ac98fd09988ee038b8f32100be
Kuan-Hao Chao <ntueeb05howard@gmail.com>
2020-03-06 15:27:42 +0800

    Update virus website url

--- VirusRNASeq/dataanalysis/urls.py
+++ VirusRNASeq/dataanalysis/urls.py
@@ -6,17 +6,34 @@ from dataanalysis import views
 urlpatterns = [
     path('<slug:slug_project>/reference-mapping/settings', views.reference_mapping_whole_dataanalysis, name='reference_mapping_dataanalysis_home'),
     path('<slug:slug_project>/de-novo-assembly/settings', views.de_novo_assembly_whole_dataanalysis, name='de_novo_assembly_dataanalysis_home'),
+
+    path('<slug:slug_project>/virus-assembly/settings', views.virus_assembly_whole_dataanalysis, name='virus_dataanalysis_home'),
+
     path('<slug:slug_project>/reference-mapping/data-upload', views.BasicUploadView.as_view(), name='reference_mapping_dataanalysis_data_upload'),
     path('<slug:slug_project>/de-novo-assembly/data-upload', views.BasicUploadView.as_view(), name='de_novo_assembly_dataanalysis_data_upload'),
+
+    path('<slug:slug_project>/virus-assembly/data-upload', views.BasicUploadView.as_view(), name='virus_assembly_dataanalysis_data_upload'),
+
+
     path('result/<slug:slug_project>/', views.show_result, name="dataanalysis_result"),
+
+
     path('result/<slug:slug_project>/reference-mapping/overview/',
          views.reference_mapping_show_result_overview, name="reference_mapping_dataanalysis_result_overview"),
     path('result/<slug:slug_project>/de-novo-assembly/overview/',
          views.de_novo_assembly_show_result_overview, name="de_novo_assembly_dataanalysis_result_overview"),
+
+    path('result/<slug:slug_project>/virus-assembly/overview/',
+         views.virus_show_result_overview, name="virus_assembly_dataanalysis_result_overview"),
+
+
     path('result/<slug:slug_project>/reference-mapping/current-status/',
          views.reference_mapping_current_status, name="reference_mapping_dataanalysis_result_current_status"),
     path('result/<slug:slug_project>/de-novo-assembly/current-status/',
          views.de_novo_assembly_current_status, name="de_novo_assembly_dataanalysis_result_current_status"),
+
+    path('result/<slug:slug_project>/virus-assembly/current-status/',
+         views.virus_assembly_current_status, name="virus_assembly_dataanalysis_result_current_status"),
 
 
     path('result/<slug:slug_project>/<slug:slug_sample>/reference-mapping/current-status/QC/pre/multiqc_before_report', views.pre_qc_html_view_multiqc, name="reference_mapping_dataanalysis_result_current_status_pre_multiqc_html"),
